2b2245c58d53a3f90273cb76bba9deba331dea37
Daria Skorobogatova <daria.skorobogatova@yandex.ru>
2023-12-11 21:07:07 +0300

    init commit

--- tests/test_send_form.py
+++ tests/test_send_form.py
@@ -1,40 +1,46 @@
-from selene import browser, by, have, be
+from selene import browser, have, command
 import os
+import tests
 
 
 def test_send_form(browser_configs):
-    browser.open('/automation-practice-form')
-    browser.element('#firstName').type('Ada')
-    browser.element('#lastName').type('Lovelace')
-    browser.element('#userEmail').type('lovelace@rumbler.ru')
-    browser.element('//div[@id="genterWrapper"]/div[2]/div[2]').click()
-    browser.element('#userNumber').type('1112223344')
-    browser.element('#dateOfBirthInput').click()
-    browser.element('[value="11"]').click()
-    browser.element('[value="2000"]').click()
-    browser.element('[aria-label*="22nd"]').click()
-    browser.element('#subjectsInput').type('economics')
-    browser.element('#react-select-2-option-0').click()
-    browser.element('[for="hobbies-checkbox-1"]').click()
-    browser.element('[for="hobbies-checkbox-2"]').click()
-    browser.element('[for="hobbies-checkbox-3"]').click()
-    browser.element('#uploadPicture').send_keys(os.path.abspath('../pictures/flower.png'))
-    browser.element('#currentAddress').type('Test street, 9-99')
-    browser.element('#react-select-3-input').type('Uttar Pradesh').press_enter()
-    browser.element('#react-select-4-input').type('Lucknow').press_enter()
-    browser.element('#submit').press_enter()
-    # Почему press_enter() на кнопке submit срабатывает, а click() - нет?
-    browser.element('.modal-header').should(have.text('Thanks for submitting the form'))
-    browser.all('.table>tbody>tr>td:nth-of-type(even)').should(have.texts(
-        'Ada Lovelace',
-        'lovelace@rumbler.ru',
-        'Female',
-        '1112223344',
-        '22 December,2000',
-        'Economics',
-        'Sports, Reading, Music',
-        'flower.png',
-        'Test street, 9-99',
-        'Uttar Pradesh Lucknow'
-    ))
-    browser.element('#closeLargeModal').press_enter()
+    browser.open("/automation-practice-form")
+    browser.element("#firstName").type("Ada")
+    browser.element("#lastName").type("Lovelace")
+    browser.element("#userEmail").type("lovelace@rumbler.ru")
+    browser.element("[name=gender][value=Female]+label").click()
+    browser.element("#userNumber").type("1112223344")
+    browser.element("#dateOfBirthInput").click()
+    browser.element('.react-datepicker__month-select').type('December')
+    browser.element('.react-datepicker__year-select').type('2000')
+    browser.element(f'.react-datepicker__day--0{22}').click()
+    browser.element("#subjectsInput").type("economics").press_enter()
+    browser.element("[for=hobbies-checkbox-2]").perform(command.js.scroll_into_view)
+    browser.all('.custom-checkbox').element_by(have.exact_text('Reading')).click()
+    browser.element("#uploadPicture").set_value(
+        os.path.abspath(
+            os.path.join(os.path.dirname(tests.__file__), "../resourses/flower.png")
+        )
+    )
+    browser.element("#currentAddress").type("Test street, 9-99")
+    browser.element('#state').click()
+    browser.all('[id^=react-select][id*=option]').element_by(have.exact_text('Uttar Pradesh')).click()
+    browser.element('#city').click()
+    browser.all('[id^=react-select][id*=option]').element_by(have.exact_text('Lucknow')).click()
+    browser.element("#submit").press_enter()
+    browser.element(".modal-header").should(have.text("Thanks for submitting the form"))
+    browser.element(".table").all("td:nth-of-type(even)").should(
+        have.texts(
+            "Ada Lovelace",
+            "lovelace@rumbler.ru",
+            "Female",
+            "1112223344",
+            "22 December,2000",
+            "Economics",
+            "Reading",
+            "flower.png",
+            "Test street, 9-99",
+            "Uttar Pradesh Lucknow",
+        )
+    )
+    browser.element("#closeLargeModal").press_enter()
