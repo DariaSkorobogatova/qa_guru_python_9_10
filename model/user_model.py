5eb33f11b3b89eb0b5b6cbc448e07c9afbf5f609
Daria Skorobogatova <daria.skorobogatova@yandex.ru>
2024-01-03 17:27:40 +0300

    changed user model

--- model/user_model.py
+++ model/user_model.py
@@ -3,27 +3,37 @@ import dataclasses
 
 @dataclasses.dataclass
 class User:
-    full_name: str
+    first_name: str
+    last_name: str
     email: str
     gender: str
     phone_number: str
-    date_of_birth: str
+    bd_month: str
+    bd_year: str
+    bd_day: str
     subject: str
     hobby: str
     avatar: str
     current_address: str
-    city: str
+    user_state: str
+    user_city: str
 
 
-user = User(
-    full_name="Ada Lovelace",
+test_user = User(
+    first_name='Ada',
+    last_name='Lovelace',
     email="lovelace@rumbler.ru",
     gender="Female",
     phone_number="1112223344",
-    date_of_birth="22 December,2000",
+    bd_month='December',
+    bd_year='2000',
+    bd_day='22',
     subject="Economics",
     hobby="Reading",
     avatar="flower.png",
-    current_address="Test street, 9-99",
-    city="Uttar Pradesh Lucknow",
+    current_address='Test street, 9-99',
+    user_state='Uttar Pradesh',
+    user_city='Lucknow',
 )
+
+
